b7e29082cf56f9433f387f51207837a4c06fd85e
JeeLee <18810996718@163.com>
2017-10-04 00:05:18 +0800

    update cookie

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,5 +1,39 @@
 cookie，localStorage和sessionStorage的区别
 1.容量：cookie存储量太小，只有4KB；HTML5专门为存储设计，最大容量为5M
 2.是否会携带到ajax中：cookie所有HTTP请求都带着，会影响获取资源的效率
-3.API易用性：cookie的API简单，需要封装才能用document.cookie = ...；localStorage.setItem(key,value);
+3.API易用性：cookie的API简单，需要封装才能用document.cookie = ...；
+						 localStorage.setItem(key,value);
 						 localStorage.getItem(key)
+
+
+//设置cookie封装
+function setCookie(name, value, iDay) {
+	var oDate = new Date();
+	oDate.setDate(oDate.getDate() + iDay);
+
+	document.cookie = name + '=' + value + ';expires=' + oDate;
+}						 
+
+setCookie('userName','lijin',365);
+setCookie('password','123456',14);
+
+//获取cookie封装
+function getCookie(name) {
+	var arr = document.cookie.split('; ');
+
+	for(var i=0;i<arr.length;i++){
+		var arr2 = arr[i].split('=');
+
+		for(arr2[0] == name) {
+			return arr2[1]
+		}
+	}
+
+	return '';	
+}
+
+
+//删除cookie封装
+function removeCookie(name) {
+	setCookie(name,1,-1);
+}
