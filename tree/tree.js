f22551289d2ec1ead9cdd0f0cec3b2d37f028050
JeeLee <18810996718@163.com>
2017-10-10 11:03:33 +0800

    update tree

--- tree/tree.js
+++ tree/tree.js
@@ -31,10 +31,61 @@ function BinaryTree() {
 			insertNode(root, newNode)
 		}
 	}
+
+	var inOrderTraverseNode = function(node,callback) {
+		if(node !== null) {
+			callback(node.key);
+			inOrderTraverseNode(node.left,callback);
+			
+			inOrderTraverseNode(node.right,callback)
+		}
+	}
+
+	this.inOrderTraverse = function(callback) {
+		inOrderTraverseNode(root,callback)
+	}
+
+	var minNode = function(node) {
+		if(node) {
+			while (node && node.left !== null) {
+				node = node.left
+			}
+
+			return node.key
+		}
+
+		return null;
+	}
+
+	this.min = function() {
+		return minNode(root);
+	}
+
+	var maxNode = function(node) {
+		if(node) {
+			while (node && node.right !== null) {
+				node = node.right
+			}
+
+			return node.key
+		}
+	}
+
+	this.max = function() {
+		return maxNode(root)
+	}
 }	
 
-	var nodes = [8,5,4,3,67,23,787,46,87,23,1];
-	var binaryTree = new BinaryTree();
-	nodes.forEach(function(key){
-		console.log(binaryTree.insert(key));
-	})
+var nodes = [8,5,4,3,67,23,787,46,87,23,1];
+var binaryTree = new BinaryTree();
+nodes.forEach(function(key){
+	binaryTree.insert(key);
+})
+
+var callback = function(key) {
+	console.log(key)
+}
+
+binaryTree.inOrderTraverse(callback)
+console.log("min node is:" + binaryTree.min())
+console.log("max node is:" + binaryTree.max())
